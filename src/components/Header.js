c274b7eeef7783b5430281717b4c733a9e19954a
Nathan Swindall <Nathan.Swindall20103@gmail.com>
2020-12-08 09:14:02 -0600

    made expensify better

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { NavLink} from 'react-router-dom';
 
-
 const Header = () => (
     <header>
-        <h1>Portfolio App</h1>
+        <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Home Page</NavLink>
-        <NavLink to="/portfolio" activeClassName="is-active" exact={true}>portfolio</NavLink>
-        <NavLink to="/contact" activeClassName="is-active">contact</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Page</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit Page</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help Page</NavLink>
     </header>
 )
 
